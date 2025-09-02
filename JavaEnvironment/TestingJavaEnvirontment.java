76434aaf4d4836ec36cf691afe6c58bc5c4fae0f
zidanYourBae <150268415+fleajion@users.noreply.github.com>
2025-09-03 02:53:15 +0700

    Add files via upload

---
+++ JavaEnvironment/TestingJavaEnvirontment.java
@@ -0,0 +1,5 @@
+public class TestingJavaEnvirontment {
+    public static void main(String[] args) {
+        System.out.println("Tugas Pratikum Membuat JAVA Environment pada matakuliah pengujian perangkat lunak!");
+    }
+}
